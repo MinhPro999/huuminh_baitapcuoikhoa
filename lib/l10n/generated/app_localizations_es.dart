6e365b2f7b63960057c373f8766085e425896de8
MinhPro999 <huuminh.cv@gmail.com>
2024-11-04 00:21:38 +0700

    bai tap cuoi khoa

---
+++ lib/l10n/generated/app_localizations_es.dart
@@ -0,0 +1,53 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get helloWorld => '¡Hola Mundo!';
+
+  @override
+  String get helloVTI => 'Hola VTI, Paju Dora?';
+
+  @override
+  String get title_appbar => 'Pantalla';
+
+  @override
+  String get info_profile => 'Personal';
+
+  @override
+  String get info_name => 'Nombre completo';
+
+  @override
+  String get info_dob => 'Cumpleanos';
+
+  @override
+  String get info_phone => 'Numero';
+
+  @override
+  String get info_email => 'Correo';
+
+  @override
+  String get info_gen => 'Sexo';
+
+  @override
+  String get info_gen_m => 'Hombre';
+
+  @override
+  String get info_gen_f => 'Femenino';
+
+  @override
+  String get info_gen_o => 'Otro';
+
+  @override
+  String get info_save => 'MANTENER';
+
+  @override
+  String get info_setting => 'Configuracion';
+
+  @override
+  String get info_languege => 'Idioma';
+}
